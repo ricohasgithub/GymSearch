67a73c32f467803b5b198aac337c05a9cce06f04
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-07-19 21:53:28 -0400

    Added Constructors

--- js/gym.js
+++ js/gym.js
@@ -1,3 +1,4 @@
 
 // This file will store the object representation of a gym
 
+// Constructor
--- js/user.js
+++ js/user.js
@@ -1,2 +1,11 @@
 
 // This file will store the object representation of a user
+
+class User {
+
+    constructor(username) {
+        // Assign instance variables
+        this.username = username;
+    }
+
+}
